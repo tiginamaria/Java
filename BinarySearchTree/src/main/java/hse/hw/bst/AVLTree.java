5f2cbaa9488d740e742019347ac858f8e30c2561
Maria Tigina <tigmah@mail.ru>
2019-02-02 12:22:14 +0300

    added nested class TreeNode

--- BinarySearchTree/src/main/java/hse/hw/bst/AVLTree.java
+++ BinarySearchTree/src/main/java/hse/hw/bst/AVLTree.java
@@ -1,5 +1,77 @@
 package hse.hw.bst;
 
-public class AVLTree {
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.TreeSet;
 
+public class AVLTree<E> extends AbstractSet<E> implements MyTreeSet<E> {
+
+    private static class TreeNode<E> {
+        private TreeNode left;
+        private TreeNode right;
+        private TreeNode parent;
+        private E value;
+
+        private TreeNode(E value, TreeNode left, TreeNode right, TreeNode parent) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public TreeSet<E> descendingSet() {
+        return null;
+    }
+
+    /**
+     * Find minimum in set
+     * @return if set is not empty - least element, otherwise null
+     */
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
