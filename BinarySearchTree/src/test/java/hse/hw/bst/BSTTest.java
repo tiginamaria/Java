61f74a3238608f20201e366137661da7b75493cf
Maria Tigina <tigmah@mail.ru>
2019-02-03 16:29:02 +0300

    added test for method last

--- BinarySearchTree/src/test/java/hse/hw/bst/BSTTest.java
+++ BinarySearchTree/src/test/java/hse/hw/bst/BSTTest.java
@@ -55,6 +55,16 @@ class BSTTest {
 
     @Test
     void last() {
+        assertTrue(treeSet.add(3));
+        assertTrue(treeSet.add(2));
+        assertTrue(treeSet.add(1));
+        assertTrue(treeSet.add(4));
+        assertTrue(treeSet.add(5));
+        assertTrue(treeSet.add(6));
+        assertTrue(treeSet.add(-8));
+        assertTrue(treeSet.add(-7));
+        assertTrue(treeSet.add(-6));
+        assertEquals((Integer)(6), treeSet.last());
     }
 
     @Test
