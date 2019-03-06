0309cadaa32a14bbb29867aa6ab9bdbe6a58239a
Maria Tigina <tigmah@mail.ru>
2019-03-06 15:47:44 +0300

    started implementation

--- Injector/src/main/java/ru/hse/injector/Injector.java
+++ Injector/src/main/java/ru/hse/injector/Injector.java
@@ -1,16 +1,48 @@
 package ru.hse.injector;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class Injector {
+
+    private static HashMap<Class<?>, Object> createdClasses = new HashMap<>();
+
+    private static ArrayList<Class<?>> toCreateClasses = new ArrayList<>();
+
+    private static ArrayList<Class<?>> implementations = new ArrayList<>();
+
+   // private HashMap<Class<?>>
 
     /**
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        Class<?> className = Class.forName(rootClassName);
+
+        for (var impl : implementationClassNames) {
+            implementations.add(Class.forName(impl));
+        }
+
+        return create(className);
+    }
+
+    private static Object create(Class<?> className) throws InjectionCycleException, NoSuchMethodException {
+
+        if (createdClasses.containsKey(className)) {
+            return createdClasses.get(className);
+        }
+
+        if (toCreateClasses.contains(className)) {
+            throw new InjectionCycleException();
+        }
+
+        toCreateClasses.add(className);
+
+        var constructor = className.getConstructor();
+
     }
 }
