ca2ff43cf248b5bd5781d14d836f99a17618b7f0
Maria Tigina <tigmah@mail.ru>
2019-03-08 16:22:15 +0300

    added drafts for basic methods

--- Reflector/src/main/java/ru/hse/hw/Reflector.java
+++ Reflector/src/main/java/ru/hse/hw/Reflector.java
@@ -2,10 +2,10 @@ package ru.hse.hw;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.io.Serializable;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /*
@@ -30,32 +30,113 @@ import java.lang.reflect.Modifier;
  */
 public class Reflector {
 
-    void printStructure(Class<?> someClass) {
-        try(FileWriter fout = new FileWriter( "someClass.java", false))
-        {
+    private static FileWriter out;
 
-            //исследоваие модификатора
-            int mods = someClass.getModifiers();
-            if (Modifier.isPublic(mods)) {
-                System.out.println("public");
-            }
-            if (Modifier.isAbstract(mods)) {
-                System.out.println("abstract");
-            }
-            if (Modifier.isFinal(mods)) {
-                System.out.println("final");
-            }
+    public static void printStructure(Class<?> someClass) throws IOException {
 
+        try(FileWriter fileOutputStream = new FileWriter( someClass.getSimpleName() + ".java", false)) {
+            out = fileOutputStream;
+            printModifiers(someClass);
+            printClassName(someClass);
+            printTypeParameters(someClass);
+            printExtendedClass(someClass);
+            printImplementedInterfaces(someClass);
+            out.write("{\n");
+            for (var subclass : someClass.getDeclaredClasses()) {
+                printStructure(subclass);
+            }
+            printFields(someClass);
+            printMethods(someClass);
+        }
+    }
+
+    private static void printFields(Class<?> someClass) throws IOException {
+        Field[] fields = someClass.getDeclaredFields();
+        for (var field : fields) {
+            out.write(Modifier.toString(field.getModifiers()) + " ");
+            out.write(getTypeName(field.getGenericType()) + " ");
+            out.write(field.getName()+ "\n");
+        }
+    }
+
+    private static void printMethods(Class<?> someClass) throws IOException {
+        Method[] methods = someClass.getMethods();
+        for (var method : methods) {
+            out.write(method.getReturnType().getName() + " ");
+            out.write(method.getName() + "(");
+            Parameter[] parameters = method.getParameters();
+            if (parameters.length > 0) {
+                out.write(getTypeName(parameters[0].getType()) + " " + parameters[0].getName());
+                for (int i = 1; i < parameters.length; i++) {
+                    out.write(", " + getTypeName(parameters[i].getType()) + " " + parameters[i].getName());
+                }
+            }
+            out.write(") " + "{ }" + "\n");
+        }
+    }
+
+    private static void printModifiers(Class<?> someClass) throws IOException {
+        var mods = someClass.getModifiers();
+        out.write(Modifier.toString(mods));
+        if (mods != 0) {
+            out.write(" ");
+        }
+    }
+
+    private static void printClassName(Class<?> someClass) throws IOException {
+        out.write(someClass.getSimpleName());
+    }
+
+    private static void printTypeParameters(Class<?> someClass) throws IOException {
+        var typeParameters = someClass.getTypeParameters();
+        if (typeParameters.length > 0) {
+            out.write("<");
+            out.write(typeParameters[0].getName());
+            for (int i = 1; i < typeParameters.length; i++) {
+                out.write(", " + typeParameters[i].getName());
+            }
+            out.write(">");
+            out.write(" ");
+        }
+    }
+
+    private static String getTypeName(Type type) {
+        return type.getTypeName().replace('$', '.');
+    }
+
+    private static void printImplementedInterfaces(Class<?> someClass) throws IOException {
+        Type[] interfaces = someClass.getGenericInterfaces();
+        if (interfaces.length > 0) {
+            out.write("implements ");
+            out.write(getTypeName(interfaces[0]));
+            for (int i = 1; i < interfaces.length; i++) {
+                out.write(", " + getTypeName(interfaces[i]));
+            }
+            out.write(" ");
+        }
+    }
+
+    private static void printExtendedClass(Class<?> someClass) throws IOException {
+        Type ancestor = someClass.getGenericSuperclass();
+        if (ancestor != null) {
+            out.write("extends " + getTypeName(ancestor));
+            out.write(" ");
+        }
+    }
+
+
+
+        void help(Class<?> someClass) {
             //интерфейсы
             Class[] interfaces = someClass.getInterfaces();
             for(Class cInterface : interfaces) {
-                System.out.println( cInterface.getName() );
+                System.out.println( cInterface.getName()+ "\n");
             }
 
 
             var classes = someClass.getClasses();
             for (var c : classes) {
-                fout.write(c.getSimpleName());
+                System.out.println(c.getSimpleName()+ "\n");
             }
 
             //поля
@@ -63,11 +144,11 @@ public class Reflector {
             Field[] publicFields = c.getFields();
             for (Field field : publicFields) {
                 Class fieldType = field.getType();
-                System.out.println("Имя: " + field.getName());
-                System.out.println("Тип: " + fieldType.getName());
+                System.out.println("Имя: " + field.getName()+ "\n");
+                System.out.println("Тип: " + fieldType.getName()+ "\n");
             }
 
-            //field.set(2, "New name");
+            //field.set(2, "New name\n");
 
 
             //конструктор
@@ -76,35 +157,29 @@ public class Reflector {
             for (Constructor constructor : constructors) {
                 Class[] paramTypes = constructor.getParameterTypes();
                 for (Class paramType : paramTypes) {
-                    System.out.print(paramType.getName() + " ");
+                    System.out.println(paramType.getName() + " \n");
                 }
-                System.out.println();
             }
 
             var fields = someClass.getDeclaredFields();
             for (var f : fields) {
-                fout.write(f.getName());
+                System.out.println(f.getName()+ "\n");
             }
-            fout.flush();
+            System.out.flush();
 
             //методы
             Method[] methods = c.getMethods();
             for (Method method : methods) {
-                System.out.println("Имя: " + method.getName());
-                System.out.println("Возвращаемый тип: " + method.getReturnType().getName());
+                System.out.println("Имя: " + method.getName()+ "\n");
+                System.out.println("Возвращаемый тип: " + method.getReturnType().getName() + "\n");
 
                 Class[] paramTypes = method.getParameterTypes();
-                System.out.print("Типы параметров: ");
+                System.out.println("Типы параметров: \n");
                 for (Class paramType : paramTypes) {
-                    System.out.print(" " + paramType.getName());
+                    System.out.println(" " + paramType.getName()+ "\n");
                 }
-                System.out.println();
             }
         }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
 
     void diffClasses(Class<?> a, Class<?> b) {
 
--- Reflector/src/test/java/ru/hse/hw/ReflectorTest.java
+++ Reflector/src/test/java/ru/hse/hw/ReflectorTest.java
@@ -1,18 +1,24 @@
 package ru.hse.hw;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.Writer;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReflectorTest {
 
-
     Reflector reflector = new Reflector();
-    class Printer<T> implements Serializable {
+
+    public static class Printer<T> {
+
+    }
+
+    public static class PrettyPrinter<T> extends Printer<T> implements Serializable, Runnable {
         public final static int version = 10;
 
         private T data;
@@ -25,15 +31,15 @@ class ReflectorTest {
 
         public void printData() {}
 
-        class PrettyPrinter extends Printer {
-            public void prettyPrintData() {}
+        @Override
+        public void run() {
+
         }
     }
 
     @Test
-    void printStructure() {
-        var printer = new Printer<String>();
-        reflector.printStructure(printer.getClass());
+    void printStructure() throws IOException {
+        reflector.printStructure(PrettyPrinter.class);
     }
 
     @Test
