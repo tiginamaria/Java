3f809da5399961e78052c045e01c8a8cf7a1d78e
Maria Tigina <tigmah@mail.ru>
2019-03-13 17:09:16 +0300

    project started

--- Test3/src/main/java/ru/hse/test3/LinkedHashMap.java
+++ Test3/src/main/java/ru/hse/test3/LinkedHashMap.java
@@ -1,7 +1,6 @@
 package ru.hse.test3;
 
 import java.util.AbstractMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,21 +16,37 @@ public class LinkedHashMap<K, V> extends AbstractMap<K, V> implements Map<K, V> 
         }
     }
 
-    private HashTable<K, ListNode>  hashTable;
-
-    private LinkedList<K, ListNode> listOrder;
-
     private ListNode tail;
 
     private ListNode head;
 
-    LinkedHashMap() {
-        hashTable = new HashTable<K, >();
+    private int size;
+    private int capacity;
+    private LinkedList<K, ListNode>[] table;
+
+
+    private LinkedList<K, ListNode> listOrder;
+
+
+    /**
+     * Constructor for LinkedHashTable with default capacity = 1
+     */
+    public LinkedHashMap() {
+        this(1);
     }
 
-    @Override
-    public int size() {
-        return hashTable.getSize();
+
+    /**
+     * Constructor for LinkedHashTable with given capacity
+     * @param capacity capacity
+     */
+    public LinkedHashMap(int capacity) {
+        this.capacity = capacity;
+        table = (LinkedList<K, ListNode>[]) new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new LinkedList<>();
+        }
+        listOrder = new LinkedList<>();
     }
 
     @Override
@@ -44,27 +59,126 @@ public class LinkedHashMap<K, V> extends AbstractMap<K, V> implements Map<K, V> 
         return super.containsKey(key);
     }
 
-    @Override
-    public V get(Object key) {
-        return super.get(key);
+    public int getSize() {
+        return size;
     }
 
-    private addListOrder(newNode)
-
-    @Override
-    public V put(K key, V value) {
-        var newNode = new ListNode(key, value);
-        if (head == null) {
-            head = newNode;
-            tail = head;
+    /**
+     * Double the capacity of LinkedHashTable
+     */
+    private void extend() {
+        int newCapacity = capacity * 2;
+        var newTable = new LinkedList[newCapacity];
+        for (int i = 0; i < newCapacity; i++) {
+            newTable[i] = new LinkedList();
         }
-        listOrder.put(key, newNode);
-        hashTable.put(key, newNode);
+
+        for (int i = 0; i < capacity; i++) {
+            while (!table[i].empty()) {
+                Data<K, V> elem = table[i].removeFromHead();
+                newTable[getHash(elem.getKey(), newCapacity)].put(elem.getKey(), elem.getValue());
+            }
+        }
+        capacity = newCapacity;
+        table = newTable;
+    }
+
+    /**
+     * Calculate hash for given key
+     * @param key given key of element
+     * @param mod maximum hash for key
+     * @return hash for given key
+     * @throws IllegalArgumentException - throws exception then given key is null
+     */
+    private int getHash(K key, int mod) throws IllegalArgumentException {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null!");
+        }
+        int hash = key.hashCode() % mod;
+        if (hash < 0) {
+            hash += mod;
+        }
+        return hash;
+    }
+
+    /**
+     * Check if there is an element with given key in LinkedHashTable
+     * @param key key to check
+     * @return true - if found an element with given key, false - if not
+     * @throws IllegalArgumentException - throws exception then given key is null
+     */
+    public boolean contains(K key) throws IllegalArgumentException {
+        return get(key) != null;
+    }
+
+    /**
+     * Get the value of element with given key from LinkedHashTable if it exists
+     * @param key key of element, from which the desired value can be got
+     * @return value of the element with given key or null if it does not exist
+     * @throws IllegalArgumentException - throws exception then given key is null
+     */
+    public V get( key) throws IllegalArgumentException {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null!");
+        }
+
+        return table[getHash(key, capacity)].get(key);
     }
 
     @Override
-    public V remove(Object key) {
-        return super.remove(key);
+    /**
+     * Change the value of element with given key if it exists or create element with given key and value
+     * @param key key of element where to change the value
+     * @param value new value
+     * @return Previous value of the element with given key or null if it does not exist
+     * @throws IllegalArgumentException throws exception then given key is or value is null
+     */
+    public Data<K, V> put(K key, V value) throws IllegalArgumentException {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null!");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("value can not be null!");
+        }
+
+        var oldData = table[getHash(key, capacity)].put(key, value);
+        if (oldData == null) {
+            size++;
+        }
+        if (size >= capacity) {
+            extend();
+        }
+        return oldData;
+    }
+
+    /**
+     * Remove element with given key from LinkedHashTable if it exists
+     * @param key key to remove
+     * @return Previous value of the element with given key or null if it does not exist
+     * @throws IllegalArgumentException throws exception then given key is null
+     */
+    public V remove(Object key) throws IllegalArgumentException {
+        if (key == null) {
+            throw new IllegalArgumentException("key can not be null!");
+        }
+
+        var oldData = table[getHash(key, capacity)].remove(key);
+        if (oldData != null) {
+            size--;
+        }
+        return oldData;
+    }
+
+    /**
+     * Remove all elements from LinkedHashTable and shorten capacity to default
+     */
+    public void clear() {
+        size = 0;
+        capacity = 1;
+        table = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new LinkedList<>();
+        }
     }
 
     @Override
