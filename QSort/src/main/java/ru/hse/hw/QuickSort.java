102c1987a821415af33e73a649ab3c206fa9a888
Maria Tigina <tigmah@mail.ru>
2019-03-15 22:54:01 +0300

    fixed thread

--- QSort/src/main/java/ru/hse/hw/QuickSort.java
+++ QSort/src/main/java/ru/hse/hw/QuickSort.java
@@ -1,16 +1,151 @@
 package ru.hse.hw;
 
-import java.util.concurrent.ForkJoinPool;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class QuickSort {
 
     /**
-     * Invokes a ForkJoinQuickSortTask in the provided pool, blocking until done.
+     * Generator for random value
+     */
+    private static Random randomize = new Random();
+
+    /**
+     * Pool of threads
+     */
+    private static ExecutorService threadPool;
+
+
+    /**
+     * Current total number of uncompleted thread tasks
+     */
+    private static AtomicInteger taskCounter;
+
+
+    /**
+     * Task to sort interval from l to r index of given array
+     * @param <T> type of elements to sort
+     */
+    private static class QuickSortTask<T extends Comparable<? super T>> implements Runnable {
+
+        /**
+         * Array to sort
+         */
+        private T[] a;
+
+        /**
+         * Left bound of interval of array to sort
+         */
+        private final int left;
+
+        /**
+         * Right bound of interval of array to sort
+         */
+        private final int right;
+
+        /**
+         * Primitive constructor for qSort task
+         *
+         * @param a
+         * @param left
+         * @param right
+         */
+        private QuickSortTask(T[] a, int left, int right) {
+            this.a = a;
+            this.left = left;
+            this.right = right;
+        }
+
+        /**
+         * Constructor for first task
+         *
+         * @param a array to sort
+         */
+        public QuickSortTask(T[] a) {
+            this(a, 0, a.length - 1);
+        }
+
+        /**
+         * Function which runs partition of given interval into two parts and recursively sorts this parts of array
+         */
+        @Override
+        public void run() {
+            if (left >= right) {
+                return;
+            }
+
+            int pivotIndex = partition(a, left, right);
+
+            taskCounter.addAndGet(2);
+            threadPool.execute(new QuickSortTask<>(a, left, pivotIndex));
+            threadPool.execute(new QuickSortTask<>(a, pivotIndex + 1, right));
+            if (taskCounter.decrementAndGet() == 0) {
+                synchronized (taskCounter) {
+                    taskCounter.notify();
+                }
+            }
+        }
+    }
+
+    /**
+     * Divide elements of given array into two parts -
+     * @param a
+     * @param left
+     * @param right
+     * @param <T>
+     * @return
+     */
+    private static <T extends Comparable<? super T>> int partition(T[] a, int left, int right) {
+        T pivotValue = a[middleIndex(left, right)];
+        while (left < right) {
+            while (a[left].compareTo(pivotValue) < 0) {
+                left++;
+            }
+
+            while (a[right].compareTo(pivotValue) > 0) {
+                right--;
+            }
+
+            if (left < right) {
+                T tmp = a[left];
+                a[left] = a[right];
+                a[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return right;
+    }
+
+    private static int middleIndex(int left, int right) {
+        return left + randomize.nextInt(right - left);
+    }
+
+    /**
+     * Runs a QuickSortTask for whole given array.
      * @param a array to sort
      * @return sorted array
      */
-    public static <T extends Comparable<? super T>> void forkJoinQuickSort(T[] a) {
-        ForkJoinPool pool = new ForkJoinPool();
-        pool.invoke(new ForkJoinQuickSortTask<>(a));
+    public static <T extends Comparable<? super T>> void quickSort(T[] a) throws InterruptedException {
+        quickSort(a, Runtime.getRuntime().availableProcessors());
+    }
+
+    /**
+     * Sets the maximum number of threads and runs a QuickSortTask for whole given array.
+     * @param a array to sort
+     * @return sorted array
+     */
+    public static <T extends Comparable<? super T>> void quickSort(T[] a, int threadCounter) throws InterruptedException {
+        threadPool = Executors.newFixedThreadPool(threadCounter);
+        taskCounter = new AtomicInteger(1);
+        threadPool.execute(new QuickSortTask<>(a));
+        synchronized (taskCounter) {
+            while (taskCounter.get() > 0) {
+                taskCounter.wait();
+            }
+        }
+        threadPool.shutdown();
     }
 }
--- QSort/src/test/java/ru/hse/hw/QuickSortTest.java
+++ QSort/src/test/java/ru/hse/hw/QuickSortTest.java
@@ -2,14 +2,14 @@ package ru.hse.hw;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static ru.hse.hw.QuickSort.quickSort;
 
 class QuickSortTest {
 
     @Test
-    void latchQuickSort() {
+    void quickSortTest() throws InterruptedException {
         var a = new Integer[]{5, 3, 5, 4 , 1};
-        QuickSort.forkJoinQuickSort(a);
+        quickSort(a, 3);
         for (var x : a) {
             System.out.println(x);
         }
