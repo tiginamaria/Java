57d12d4e7b48842485328528cc73e5de39275feb
Maria Tigina <tigmah@mail.ru>
2019-03-15 23:50:02 +0300

    fixed thread

--- QSort/src/main/java/ru/hse/hw/QuickSort.java
+++ QSort/src/main/java/ru/hse/hw/QuickSort.java
@@ -17,6 +17,10 @@ public class QuickSort {
      */
     private static Random randomize = new Random();
 
+    /**
+     * Stores general information common for all threads
+     * @param <T> type of sorted elements
+     */
     private static class GeneralTaskTools<T> {
         /**
          * Pool of threads
@@ -26,18 +30,22 @@ public class QuickSort {
         /**
          * Current total number of uncompleted thread tasks
          */
-        private AtomicInteger taskCounter;
+        private final AtomicInteger taskCounter;
 
+        /**
+         * Comparator for elements in sorted array
+         */
         private Comparator<? super T> comparator;
 
         /**
-         * Constructor sets common for all threads comparator, thead pool and number of current tasks;
-         * @param threadPool
-         * @param taskCounter
+         * Constructor sets common for all threads comparator, thead pool and number of current tasks
+         * @param threadPool thread pool
+         * @param taskCounter and number of current tasks
          */
-        private GeneralTaskTools(ExecutorService threadPool, AtomicInteger taskCounter) {
+        private GeneralTaskTools(ExecutorService threadPool, AtomicInteger taskCounter, Comparator<? super T> comparator) {
             this.threadPool = threadPool;
             this.taskCounter = taskCounter;
+            this.comparator = comparator;
         }
     }
 
@@ -45,7 +53,7 @@ public class QuickSort {
      * Task to sort interval from l to r index of given array
      * @param <T> type of elements to sort
      */
-    private static class QuickSortTask<T extends Comparable<? super T>> implements Runnable {
+    private static class QuickSortTask<T> implements Runnable {
 
         /**
          * Array to sort
@@ -65,14 +73,15 @@ public class QuickSort {
         /**
          * Hold general information about tall tasks in qsort
          */
-        private GeneralTaskTools tools;
+        private GeneralTaskTools<T> tools;
 
         /**
          * Primitive constructor for qSort task
          *
-         * @param a
-         * @param left
-         * @param right
+         * @param a array to sort
+         * @param left left bound of sorting interval of array
+         * @param right right bound of sorting interval of array
+         * @param tools general tools for multithreaded sort
          */
         private QuickSortTask(T[] a, int left, int right, GeneralTaskTools tools) {
             this.a = a;
@@ -83,9 +92,8 @@ public class QuickSort {
 
         /**
          * Constructor for first task
-         *
          * @param a array to sort
-         * @param tools number of threads
+         * @param tools general tools for multithreaded sort
          */
         public QuickSortTask(T[] a, GeneralTaskTools tools) {
             this(a, 0, a.length - 1, tools);
@@ -98,7 +106,7 @@ public class QuickSort {
         public void run() {
 
             if (left < right) {
-                int pivotIndex = partition(a, left, right);
+                int pivotIndex = partition(a, left, right, tools.comparator);
                 tools.taskCounter.addAndGet(2);
                 tools.threadPool.execute(new QuickSortTask<>(a, left, pivotIndex, tools));
                 tools.threadPool.execute(new QuickSortTask<>(a, pivotIndex + 1, right, tools));
@@ -114,20 +122,20 @@ public class QuickSort {
 
     /**
      * Divide elements of given array into two parts -
-     * @param a
-     * @param left
-     * @param right
-     * @param <T>
-     * @return
+     * @param a array to do partition
+     * @param left left bound to do partition
+     * @param right right bound to do partition
+     * @param <T> type of element in array
+     * @return bound of partition
      */
-    private static <T extends Comparable<? super T>> int partition(T[] a, int left, int right) {
+    private static <T> int partition(T[] a, int left, int right, Comparator<? super T> comparator) {
         T pivotValue = a[middleIndex(left, right)];
         while (left < right) {
-            while (a[left].compareTo(pivotValue) < 0) {
+            while (comparator.compare(a[left], pivotValue) < 0) {
                 left++;
             }
 
-            while (a[right].compareTo(pivotValue) > 0) {
+            while (comparator.compare(a[right], pivotValue) > 0) {
                 right--;
             }
 
@@ -142,28 +150,41 @@ public class QuickSort {
         return right;
     }
 
+    /**
+     * Calculate index for partition
+     * @param left left bound of interval
+     * @param right right bound of interval
+     * @return pivot element
+     */
     private static int middleIndex(int left, int right) {
-        return left + randomize.nextInt(right - left + 1);
+        return left + randomize.nextInt(right - left);
     }
 
     /**
      * Runs a QuickSortTask for whole given array.
      * @param a array to sort
-     * @return sorted array
      */
     public static <T extends Comparable<? super T>> void quickSort(T[] a) throws InterruptedException {
-        quickSort(a, Runtime.getRuntime().availableProcessors());
+        quickSort(a, Comparator.naturalOrder(), Runtime.getRuntime().availableProcessors());
+    }
+
+    /**
+     * Runs a QuickSortTask for whole given array and set tre comparator.
+     * @param a array to sort
+     * @param comparator for elements in array
+     */
+    public static <T> void quickSort(T[] a, Comparator<? super T> comparator) throws InterruptedException {
+        quickSort(a, comparator, Runtime.getRuntime().availableProcessors());
     }
 
     /**
      * Sets the maximum number of threads and runs a QuickSortTask for whole given array.
      * @param a array to sort
-     * @return sorted array
      */
-    public static <T extends Comparable<? super T>> void quickSort(T[] a, int threadCounter) throws InterruptedException {
+    private static <T> void quickSort(T[] a, Comparator<? super T> comparator, int threadCounter) throws InterruptedException {
         var threadPool = Executors.newFixedThreadPool(threadCounter);
         var taskCounter = new AtomicInteger(1);
-        var tools = new GeneralTaskTools<T>(threadPool, taskCounter);
+        var tools = new GeneralTaskTools<T>(threadPool, taskCounter, comparator);
         threadPool.execute(new QuickSortTask<>(a, tools));
         synchronized (taskCounter) {
             while (taskCounter.get() > 0) {
--- QSort/src/test/java/ru/hse/hw/QuickSortTest.java
+++ QSort/src/test/java/ru/hse/hw/QuickSortTest.java
@@ -2,6 +2,8 @@ package ru.hse.hw;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+
 import static ru.hse.hw.QuickSort.quickSort;
 
 class QuickSortTest {
@@ -9,7 +11,7 @@ class QuickSortTest {
     @Test
     void quickSortTest() throws InterruptedException {
         var a = new Integer[]{5, 3, 5, 4 , 1};
-        quickSort(a, 3);
+        quickSort(a, Comparator.reverseOrder());
         for (var x : a) {
             System.out.println(x);
         }
